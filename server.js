e1c655731ef069e12a3605d9a8a1673892513ef2
Ali Ashraf <adaydreamer10@gmail.com>
2021-06-05 21:02:54 +0200

    Post - Creating new Quote with id generator

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json())
 
 const quotes = require("./quotes.json");
 
@@ -11,6 +12,8 @@ app.get("/quotes", function(request, response){
   response.status(200).send(quotes);
 });
 
+//  Get - Get quote by id
+
 app.get("/quotes/:id", function(request, response){
   const id = parseInt(request.params.id)
   const quoteByID = quotes.filter((each)=>{
@@ -19,5 +22,26 @@ app.get("/quotes/:id", function(request, response){
 
   response.status(200).send(quoteByID);
 });
+
+// Post - Create a new Object with automatic new id generator
+
+app.post("/quotes", function(request, response){
+  const lastObject = quotes[quotes.length -1]
+  const idGenerator = lastObject.id + 1
+  
+  const quote = request.body.quote
+  const author = request.body.author
+
+  const newQuote = {
+    quote: quote,
+    author: author,
+    id: idGenerator
+  }
+
+  quotes.push(newQuote)
+
+  response.status(201).send(newQuote);
+});
+
 
 app.listen(3000, () => console.log("Listening on port 3000"));
